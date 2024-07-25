302d12db6d15e5d2ef169210f96fa58d7a925426
quandeveloper03 <quandeveloper03@gmail.com>
2024-07-25 14:14:46 +0700

    build top header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Header.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            <div className="header">
+                <div className="header-top">
+                    <div className="container">
+                        <div className="header-wrapper">
+                            <div className="header-top-left">
+                                <span className="caption">Tikay Coffee - Suy hơn người yêu cũ</span>
+                            </div>
+                            <div className="header-top-right">
+                                <div><FontAwesomeIcon icon={faClock} /> 07:30 - 22:00</div>
+                                <hr />
+                                <div><FontAwesomeIcon icon={faPhone} /> +084 322 040</div>
+                                <hr />
+                                <div><FontAwesomeIcon icon={faEnvelope} /> minhq130@gmail.com</div>
+                            </div>
+                        </div>
+                        <div className="header-main"></div>
+                        <div className="header-bottom"></div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
